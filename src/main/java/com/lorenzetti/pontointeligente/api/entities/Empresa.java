9c370a236b6ea4ce076f30b8b108a02a2345e174
Tiago Lorenzetti <tiago.lorenzetti@sensedia.com>
2019-04-09 13:17:07 -0300

    Adicionado mapeamento de entities JPA

---
+++ src/main/java/com/lorenzetti/pontointeligente/api/entities/Empresa.java
@@ -0,0 +1,124 @@
+package com.lorenzetti.pontointeligente.api.entities;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import javax.annotation.Generated;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "empresa")
+public class Empresa implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4812467695033774053L;
+	
+	public Empresa() {
+		
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name = "razao_social", nullable = false)
+	private String razaoSocial;
+
+	@Column(name = "cnpj", nullable = false)
+	private String cnpj;
+
+	@Column(name = "data_criacao", nullable = false)
+	private Date dataCriacao;
+
+	@Column(name = "data_atualizacao", nullable = false)
+	private Date dataAtualizacao;
+	
+	@OneToMany(mappedBy = "empresa", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private List<Funcionario> funcionario;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	public Date getDataAtualizacao() {
+		return dataAtualizacao;
+	}
+
+	public void setDataAtualizacao(Date dataAtualizacao) {
+		this.dataAtualizacao = dataAtualizacao;
+	}
+
+	public List<Funcionario> getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(List<Funcionario> funcionario) {
+		this.funcionario = funcionario;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		this.dataAtualizacao = Calendar.getInstance().getTime();
+	}
+
+	@PrePersist
+	public void prePersist() {
+		final Date data = Calendar.getInstance().getTime();
+		
+		this.dataAtualizacao = data;
+		this.dataCriacao = data;
+	}
+
+	@Override
+	public String toString() {
+		return "Empresa [id=" + id + ", razaoSocial=" + razaoSocial + ", cnpj=" + cnpj + ", dataCriacao=" + dataCriacao
+				+ ", dataAtualizacao=" + dataAtualizacao + ", funcionario=" + funcionario + "]";
+	}
+	
+}
